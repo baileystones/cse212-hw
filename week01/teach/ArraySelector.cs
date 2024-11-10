379c326df0c212c19b466ed84160be214b2a98b1
baileystones <baileystones@gmail.com>
2024-11-10 16:57:41 -0700

    practice

--- week01/teach/ArraySelector.cs
+++ week01/teach/ArraySelector.cs
@@ -11,6 +11,24 @@ public static class ArraySelector
 
     private static int[] ListSelector(int[] list1, int[] list2, int[] select)
     {
-        return [];
+        List<int> result = new List<int>();
+
+        int array1Index = 0;
+        int array2Index = 0;
+
+        foreach (int s in select)
+        {
+            if (s == 1)
+            {
+                result.Add(list1[array1Index]);
+                array1Index++;
+            }
+            else if (s == 2)
+            {
+                result.Add(list2[array2Index]);
+                array2Index++;
+            }
+        }
+        return result.ToArray ();
     }
 }
