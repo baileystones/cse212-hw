7789f22d5e1a7f67d43825dd0328445f6f3e3e58
baileystones <baileystones@gmail.com>
2024-11-19 18:44:34 -0700

    Mystery Stacks Notes and Breakdowns

--- week02/analyze/MysteryStack1.cs
+++ week02/analyze/MysteryStack1.cs
@@ -1,13 +1,17 @@
 ﻿public static class MysteryStack1 {
     public static string Run(string text) {
+        //Creating a stack, LIFO
         var stack = new Stack<char>();
+        //Every character in the text is pushed into the stack
         foreach (var letter in text)
             stack.Push(letter);
-
+        //Start with empty string
         var result = "";
+        //As long as the stack is not empty loop runs
+        //Characters are popped from reverse order they were added
         while (stack.Count > 0)
             result += stack.Pop();
-
+        //Returns reversed input string
         return result;
     }
 }
--- week02/analyze/MysteryStack2.cs
+++ week02/analyze/MysteryStack2.cs
@@ -1,18 +1,26 @@
 ﻿public static class MysteryStack2 {
+    
+    //Makes sure string can be float
     private static bool IsFloat(string text) {
         return float.TryParse(text, out _);
     }
 
     public static float Run(string text) {
+        //Make a stack (LIFO)
         var stack = new Stack<float>();
+        //Splitting up the input string based on the space in-between
         foreach (var item in text.Split(' ')) {
             if (item == "+" || item == "-" || item == "*" || item == "/") {
+                //Make sure there are at least 2 numbers in stack 
                 if (stack.Count < 2)
                     throw new ApplicationException("Invalid Case 1!");
-
+                //Most recent number popped first
                 var op2 = stack.Pop();
+                //Second recent number
                 var op1 = stack.Pop();
+                //Declaring a result float variable 
                 float res;
+                //Do the math
                 if (item == "+") {
                     res = op1 + op2;
                 }
@@ -23,27 +31,31 @@
                     res = op1 * op2;
                 }
                 else {
+                    // Can't divide by zero
                     if (op2 == 0)
                         throw new ApplicationException("Invalid Case 2!");
 
                     res = op1 / op2;
                 }
-
+                //Results pushed back into stack
                 stack.Push(res);
             }
+            //If a valid number, converted to float and pushed to stack
             else if (IsFloat(item)) {
                 stack.Push(float.Parse(item));
             }
+            //Checks if empty string and does nothing if so
             else if (item == "") {
             }
+            //Anything else that isn't a valid operator, #, or empty string is invalid
             else {
                 throw new ApplicationException("Invalid Case 3!");
             }
         }
-
+        //The stack should have one number at the end
         if (stack.Count != 1)
             throw new ApplicationException("Invalid Case 4!");
-
+        //Final result popped and returned as result
         return stack.Pop();
     }
 }
